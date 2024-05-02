234f3ccce8368ff2a911dd15fad5d1e9d2f7216a
leandrospan <leandrospan@gmai.com>
2024-05-02 06:49:30 -0300

    Primeira subida

---
+++ src/main/java/com/mycompany/bin2dec/Bin2Dec.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bin2dec;
+
+/**
+ *
+ * @author Leandro
+ */
+public class Bin2Dec {
+
+    public static void main(String[] args) {
+        Tela t = new Tela();
+        t.setVisible(true);
+    }
+}
